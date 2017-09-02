2257ebb20df46b92223c8dd9736805473e65ce3e
Jeremy Beasley <jeremy.beasley@gmail.com>
2017-09-01 19:35:10 -0700

    fix: update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-# Get current dir (so run this script from anywhere)
 
+# Get current dir (so run this script from anywhere)
 export DOTFILES_DIR DOTFILES_CACHE DOTFILES_EXTRA_DIR
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DOTFILES_CACHE="$DOTFILES_DIR/.cache.sh"
