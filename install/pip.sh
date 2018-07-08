95dd19fde7205eac581e0a46df3fc7acc8dfdc30
Jeremy Beasley <jeremy.beasley@gmail.com>
2018-07-07 20:44:22 -0700

    feat: add default Python packages

---
+++ install/pip.sh
@@ -0,0 +1,15 @@
+if ! is-executable pip -o ! is-executable pipenv; then
+  echo "Skipped: pip (missing: brew and/or git)"
+  return
+fi
+
+sudo easy_install pip
+
+# Globally install with npm
+
+packages=(
+  requests
+  pipenv
+)
+
+sudo pip install "${packages[@]}"
